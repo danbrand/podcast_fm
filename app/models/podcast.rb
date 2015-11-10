c3100cc5f6ad18dc24f2efeb04f97520dd70085f
Daniel Brand <daniel@192-168-1-6.tpgi.com.au>
2015-11-10 14:05:13 +1100

    add episode model

--- app/models/podcast.rb
+++ app/models/podcast.rb
@@ -1,4 +1,5 @@
 class Podcast < ActiveRecord::Base
+  has_many :episodes	
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
